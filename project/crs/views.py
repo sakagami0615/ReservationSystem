06a42656392696be9ceb241554c56754a9ee9dfc
tomonori-sakagami <sakagami0615@gmail.com>
2020-07-03 01:23:31 +0900

    add,del:週カレンダー表示機能追加、不要データの削除

--- project/crs/views.py
+++ project/crs/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.views import generic
+from . import mixins
 
-# Create your views here.
+
 def test(request):
 	from django.http import HttpResponse
 	return HttpResponse('Helllo World')
+
+
+class WeekCalendar(mixins.WeekCalendarMixin, generic.TemplateView):
+
+    template_name = 'crs/week.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_week_calendar()
+        context.update(calendar_context)
+        return context
