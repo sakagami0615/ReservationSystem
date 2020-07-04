c8c880007c13c28539a48062113407721f015a40
tomonori-sakagami <sakagami0615@gmail.com>
2020-07-04 19:09:28 +0900

    add:データベースのテンプレートを作成
    (1)User <-> (多)Planのリレーションで設定

--- project/crs/models.py
+++ project/crs/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.models):
+
+	user_id = models.IntegerField(primary_key=True)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+
+	unlocker = models.BooleanField(default=False)
+
+	department = models.CharField(
+		blank=True,
+		null=True,
+		max_length=100)
+	division = models.CharField(
+		blank=True,
+		null=True,
+		max_length=100)
+	team = models.CharField(
+		blank=True,
+		null=True,
+		max_length=100)
+
+	create_date = models.DateTimeField(auto_now_add = True)
+	update_date = models.DateTimeField(auto_now = True)
+
+
+class Plan(models.models):
+
+	day = models.DateField()
+	begin_time = models.TimeField()
+	end_time = models.TimeField()
+	
+	create_date = models.DateTimeField(auto_now_add = True)
+	update_date = models.DateTimeField(auto_now = True)
+
+	user = models.ForeignKey(User, verbose_name='Plan', on_delete=models.CASCADE)
