a64ed8b0b557d3c51cbc3a7af31213174b3b4c29
tomonori-sakagami <sakagami0615@gmail.com>
2020-08-16 01:08:00 +0900

    fix: インデント修正および重複定義を削除

--- project/crs/urls.py
+++ project/crs/urls.py
@@ -7,5 +7,5 @@ app_name = 'crs'
 urlpatterns = [
 	path('test', views.test, name='test'),
 	path('week/', views.WeekCalendar.as_view(), name='week'),
-    path('week/<int:year>/<int:month>/<int:day>/', views.WeekCalendar.as_view(), name='week'),
+	path('week/<int:year>/<int:month>/<int:day>/', views.WeekCalendar.as_view(), name='week'),
 ]
